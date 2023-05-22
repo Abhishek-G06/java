70420b970e0f9142503ce72aa43781775a9449d3
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-05-22 22:28:28 +0530

    new changes

--- M5/socket2.java
+++ M5/socket2.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class socket2 {
     public static void main(String[] args) throws Exception{
-        Socket socket2 = new Socket("127.0.0.1", 0);
-        System.out.println(socket2.getPort());
+        Socket s = new Socket("127.0.0.1", 0);
+        System.out.println(s.getPort());
     }
 }
