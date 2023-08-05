abdb0d4cc50cc45f77864598322511cda1b19963
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-06 04:51:28 +0530

    check array is sorted by recursion

---
+++ SortedRecursion.java
@@ -0,0 +1,14 @@
+public class SortedRecursion {
+    public static void main(String[] args) {
+        int[] ar= {1,2,3,4,5,6,7};
+        System.out.println(sorted(ar, 0));
+    }
+
+    public static boolean sorted(int[] arr, int ind){
+
+        if(ind == arr.length-1){
+            return true;
+        }
+        return arr[ind] < arr[ind+1] && sorted(arr, ind+1);
+    }
+}
