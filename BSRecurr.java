f87274219c478b0b5517cd9b706d2cc7dac68084
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-05 16:44:00 +0530

    optimised binary search by recursion

--- BSRecurr.java
+++ BSRecurr.java
@@ -1,20 +1,18 @@
 public class BSRecurr {
     public static void main(String[] args) {
-        int[] n = {2,3,4,5,6};
-        int target = 4;
-        int s = 0;
-        int e = n.length;
-        System.out.println(BS(n,target,s,e));
+        int[] n = {2,3,4,5,6,7,8,9};
+        int target = 7;
+        System.out.println(BS(n,target,0,n.length-1));
     }
     public static int BS(int[] n ,int target, int s, int e){
-        while(s <= e){
+        while(s < e){
             int mid = s+(e-s)/2;
             if(target == n[mid]){
                 return mid;
             }else if(target<n[mid]){
-                e = mid-1;
+                return BS(n, target, s, mid-1);
             }else{
-                s = mid+1;
+                return BS(n, target, mid+1, e);
             } 
         }
         return -1;
