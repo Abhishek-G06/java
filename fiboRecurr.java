9bdce9687aa74f3ea7b6f67b9eaeb8c490da0888
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-05 16:25:19 +0530

    Fibonacci number at a position by Recursion

---
+++ fiboRecurr.java
@@ -0,0 +1,13 @@
+public class fiboRecurr {
+    public static void main(String[] args) {
+        System.out.println(fibo(8));
+    }
+
+    public static int fibo(int n){
+        //base condition
+        if (n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
