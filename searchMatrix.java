134da6cfdab51838cffb4b607cb7979c436c8e22
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-08 22:48:50 +0530

    Search in 2D Matrix

---
+++ searchMatrix.java
@@ -0,0 +1,30 @@
+public class searchMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        int target = 13;
+        searchMatrix solution = new searchMatrix();
+        System.out.println(solution.SearchMatrix(matrix, target));
+    }
+    public boolean SearchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int midValue = matrix[row][col];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
