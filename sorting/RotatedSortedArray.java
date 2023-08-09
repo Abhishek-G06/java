4f355fa35ae8a60e7ff71f1716ee67e20ebeafe1
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-09 17:42:49 +0530

    Recursion sorting

--- sorting/RotatedSortedArray.java
+++ sorting/RotatedSortedArray.java
@@ -1,3 +1,4 @@
+package sorting;
 public class RotatedSortedArray {
     public static void main(String[] args) {
         int[] nums={4,5,6,7,0,1,2};
--- sorting/SortedRecursion.java
+++ sorting/SortedRecursion.java
@@ -1,3 +1,4 @@
+package sorting;
 public class SortedRecursion {
     public static void main(String[] args) {
         int[] ar= {1,2,3,4,5,6,7};
