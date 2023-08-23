4ddb57f8d14a4eb7058448dfcb316001222eeb2d
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-23 23:44:23 +0530

    BackTracking on Maze Problem to find all Possible paths (JAVA)

---
+++ Backtracking.java
@@ -0,0 +1,37 @@
+public class Backtracking {
+    public static void main(String[] args) {
+        boolean[][] board = {
+            {true,true,true},
+            {true,true,true},
+            {true,true,true}
+        };
+
+        allPaths("", board, 0, 0);
+        
+    }
+    private static void allPaths(String p, boolean[][] maze, int r, int c) {
+        if(r == maze.length-1 && c == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+        maze[r][c] = false;
+
+        if(r > 0){
+            allPaths(p+'U', maze, r-1, c);
+        }
+        if(c > 0){
+            allPaths(p+'L', maze, r, c-1);
+        }
+        if(r < maze.length-1){
+            allPaths(p+'D', maze, r+1, c);
+        }
+        if(c < maze[0].length-1){
+            allPaths(p+'R', maze, r, c+1);
+        }
+
+        maze[r][c] = true;
+    }
+}
