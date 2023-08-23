1d8e90be02cdcb0779bacd2c7136f6b5233f1ba1
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-23 21:10:32 +0530

    Maze Problem with printing Paths (JAVA)

---
+++ Maze2.java
@@ -0,0 +1,17 @@
+public class Maze2 {
+    public static void main(String[] args) {
+        path("",4,3);
+    }
+    static void path(String p, int r, int c){
+        if(r == 1 && c == 1 ){
+            System.out.println(p);
+            return;
+        }
+        if(r > 1){
+            path(p+'D', r-1, c);
+        }
+        if(c > 1){
+            path(p+'R', r, c-1);
+        }
+    }
+}
