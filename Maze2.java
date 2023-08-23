a74a9b156b13f2ed90d623d6e77b1d79409103c4
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-08-23 21:29:01 +0530

    Printing & Counting Maze paths normally and diagonaly

--- Maze2.java
+++ Maze2.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Maze2 {
     public static void main(String[] args) {
-        path("",4,3);
+        //path("",3,3);
+        System.out.println(pathRetDigonal("", 3, 3));
     }
     static void path(String p, int r, int c){
         if(r == 1 && c == 1 ){
@@ -13,5 +16,28 @@ public class Maze2 {
         if(c > 1){
             path(p+'R', r, c-1);
         }
+    }
+    
+    // Returning Paths in list and Printing diagonal paths too
+
+    static ArrayList<String> pathRetDiagonal(String p, int r , int c){
+        if(r == 1 && c == 1 ){
+            ArrayList<String> list = new ArrayList<>(); 
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        
+        if(r > 1){
+            list.addAll(pathRetDiagonal(p+'V', r-1, c));
+        }
+        if(c > 1){
+            list.addAll(pathRetDiagonal(p+'H', r, c-1));
+        }
+        if(r > 1 && c > 1){
+            list.addAll(pathRetDiagonal(p+'D', r-1, c-1));
+        }
+        return list;
     }
 }
